8d74e2e8577c3de7e76864290bfc89ca762cb07e
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-12-16 14:39:04 +0100

    blacked

--- utils/cli.py
+++ utils/cli.py
@@ -3,6 +3,7 @@ import argparse
 REQUIRED = True
 DEFAULT_ARGUMENT = "./data"
 
+
 def default_func(path):
     return "add your own function"
 
@@ -12,16 +13,19 @@ def run(
     func=default_func,
     required=REQUIRED,
     default_argument=DEFAULT_ARGUMENT,
-    help_text="runs default function"
+    help_text="runs default function",
 ):
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--path", default=default_argument, type=str, help=help_text, required=required)
+    parser.add_argument(
+        "--path", default=default_argument, type=str, help=help_text, required=required
+    )
     args = parser.parse_args()
     return func(args.path)
 
 
 def main():
     print(run())
+
 
 if __name__ == "__main__":
     main()
--- utils/helpers.py
+++ utils/helpers.py
@@ -9,13 +9,13 @@ def get_tree(root: Path):
     tree = []
     for file in root.rglob("*"):
         if file.is_file() and not any(
-            part.startswith(".")
-            for part in file.relative_to(root).parts
+            part.startswith(".") for part in file.relative_to(root).parts
         ):
             relative_path = file.relative_to(root)
             target_file = relative_path
             tree.append(target_file)
     return tree
+
 
 if __name__ == "__main__":
     tree = get_tree(Path("data"))
